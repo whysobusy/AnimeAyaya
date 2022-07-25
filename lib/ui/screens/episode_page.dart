3b7cbdf127eb926e55b3ade9f347ff9372748ff7
whysobusy <penetrate.darkness@gmail.com>
2022-07-25 22:50:38 +0800

    episode page

--- lib/ui/screens/episode_page.dart
+++ lib/ui/screens/episode_page.dart
@@ -5,7 +5,6 @@ import 'package:anime_player/data/models/basic_anime.dart';
 import 'package:anime_player/data/models/anime_video.dart';
 import 'package:anime_player/data/models/one_episode_info.dart';
 import 'package:anime_player/ui/screens/anime_detail_page.dart';
-import 'package:anime_player/ui/screens/category_page.dart';
 import 'package:anime_player/ui/screens/loading_page.dart';
 import 'package:anime_player/ui/screens/watch_anime_page.dart';
 import 'package:anime_player/ui/widgets/search_anime_button.dart';
@@ -50,7 +49,53 @@ class _EpisodePageState extends State<EpisodePage>
                   'Episode ${state.info.currentEpisode ?? '??'}',
                 ),
               ),
-              body: renderBody(state.info),
+              body: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      ListTile(
+                        title: const Text(
+                          'Anime Detail',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.pinkAccent, fontSize: 24, fontWeight: FontWeight.w500),
+                        ),
+                        subtitle: Text(
+                          state.info.name ?? 'No information',
+                          textAlign: TextAlign.center,
+                        ),
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  AnimeDetailPage(info: state.info),
+                            ),
+                          );
+                        },
+                      ),
+                      const Text(
+                        'Please note that this app does not\nhave any controls over these sources',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white70,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      ListTile(
+                        title: const Text('Server List',
+                            textAlign: TextAlign.center),
+                        subtitle: Center(
+                          child: _ServerList(oneEpisodeInfo: state.info),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Divider(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               bottomNavigationBar: BottomAppBar(
                 child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -92,95 +137,74 @@ class _EpisodePageState extends State<EpisodePage>
       ),
     );
   }
+  //TODO
 
-  Widget renderBody(OneEpisodeInfo info) {
-    return Center(
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: const Text('Anime Info', textAlign: TextAlign.center),
-              subtitle: Text(
-                info.name ?? 'No information',
-                textAlign: TextAlign.center,
-              ),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AnimeDetailPage(info: info),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Category', textAlign: TextAlign.center),
-              subtitle: Text(
-                info.category ?? 'Unknown',
-                textAlign: TextAlign.center,
-              ),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CategoryPage(
-                      url: info.categoryLink,
-                      title: info.category,
-                    ),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Server List', textAlign: TextAlign.center),
-              subtitle: Center(
-                child: Wrap(
-                  alignment: WrapAlignment.center,
-                  children: renderServerList(info) ?? [],
-                ),
-              ),
-            ),
-            const Text(
-              'Please note that this app does not\nhave any controls over these sources',
-              textAlign: TextAlign.center,
-            ),
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Divider(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  // List<Widget>? renderServerList(OneEpisodeInfo info) {
+  //   return info.servers.map((e) {
+  //     return Padding(
+  //       padding: const EdgeInsets.only(right: 8),
+  //       child: Tooltip(
+  //         message: 'Watch on ${e.title} server',
+  //         child: ActionChip(
+  //           onPressed: () {
+  //             if (Util.isMobile()) {
+  //               openInAppPlayer(e);
+  //               _addToHistory(BasicAnime(
+  //                 info.episodeName,
+  //                 info.currentEpisodeLink,
+  //               ));
+  //             } else {
+  //               if (e.link != null) launchUrl(Uri.parse(e.link!));
+  //               _addToHistory(BasicAnime(
+  //                 info.episodeName,
+  //                 info.currentEpisodeLink,
+  //               ));
+  //             }
+  //           },
+  //           label: Text(e.title ?? 'Unknown'),
+  //         ),
+  //       ),
+  //     );
+  //   }).toList();
+  // }
 
-  List<Widget>? renderServerList(OneEpisodeInfo info) {
-    return info.servers.map((e) {
-      return Padding(
-        padding: const EdgeInsets.only(right: 8),
-        child: Tooltip(
-          message: 'Watch on ${e.title} server',
-          child: ActionChip(
-            onPressed: () {
-              if (Util.isMobile()) {
-                openInAppPlayer(e);
-                _addToHistory(BasicAnime(
-                  info.episodeName,
-                  info.currentEpisodeLink,
-                ));
-              } else {
-                if (e.link != null) launchUrl(Uri.parse(e.link!));
-                _addToHistory(BasicAnime(
-                  info.episodeName,
-                  info.currentEpisodeLink,
-                ));
-              }
-            },
-            label: Text(e.title ?? 'Unknown'),
-          ),
-        ),
-      );
-    }).toList();
+  // /// Watch with in app player
+  // openInAppPlayer(VideoServer e) {
+  //   // Only android has the
+  //   if (Util.isAndroid()) {
+  //     Navigator.pop(context);
+  //   }
+
+  //   Navigator.push(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (context) => WatchAnimePage(video: e),
+  //       fullscreenDialog: true,
+  //     ),
+  //   );
+  // }
+
+  // /// Save this to watch history
+  // _addToHistory(BasicAnime anime) {
+  //   BlocProvider.of<AppBloc>(context)
+  //       .add(AppUpdateHistory(anime: anime, add: true));
+  // }
+}
+
+class _ServerList extends StatefulWidget {
+  const _ServerList({super.key, required this.oneEpisodeInfo});
+
+  final OneEpisodeInfo oneEpisodeInfo;
+
+  @override
+  State<_ServerList> createState() => _ServerListState();
+}
+
+class _ServerListState extends State<_ServerList> {
+  /// Save this to watch history
+  _addToHistory(BasicAnime anime) {
+    BlocProvider.of<AppBloc>(context)
+        .add(AppUpdateHistory(anime: anime, add: true));
   }
 
   /// Watch with in app player
@@ -199,8 +223,45 @@ class _EpisodePageState extends State<EpisodePage>
     );
   }
 
-  /// Save this to watch history
-  _addToHistory(BasicAnime anime) {
-    BlocProvider.of<AppBloc>(context).add(AppUpdateHistory(anime: anime, add: true));
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: widget.oneEpisodeInfo.servers.map((e) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Tooltip(
+            message: 'Watch on ${e.title} server',
+            child: ActionChip(
+              shape: const StadiumBorder(
+                  side: BorderSide(color: Colors.pinkAccent)),
+              backgroundColor: Colors.transparent,
+              label: SizedBox(
+                width: 300,
+                child: Text(
+                  e.title ?? 'Unknown',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.pinkAccent),
+                ),
+              ),
+              onPressed: () {
+                if (Util.isMobile()) {
+                  openInAppPlayer(e);
+                  _addToHistory(BasicAnime(
+                    widget.oneEpisodeInfo.episodeName,
+                    widget.oneEpisodeInfo.currentEpisodeLink,
+                  ));
+                } else {
+                  if (e.link != null) launchUrl(Uri.parse(e.link!));
+                  _addToHistory(BasicAnime(
+                    widget.oneEpisodeInfo.episodeName,
+                    widget.oneEpisodeInfo.currentEpisodeLink,
+                  ));
+                }
+              },
+            ),
+          ),
+        );
+      }).toList(),
+    );
   }
 }
